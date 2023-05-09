22413a91ee9ba8355c1f8bbaac906a16709ad18f
Cjr88 <carloscjr70@gmail.com>
2023-05-09 15:44:39 -0300

    commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadButton = document.getElementById('load')
+
+const maxRecords = 151
+const limit = 10;
+let offset = 0;
+
+
+
+
+
+
+function loadPokemonItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) =>{
+        const newHtml = pokemons.map((pokemon) =>
+            `<li class="pokemon ${pokemon.type}">
+            <span class="number">${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span> 
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+            </li>`).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecord = offset + limit
+
+    if (qtdRecord >= maxRecords){
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadButton.parentElement.removeChild(loadButton)
+    }else{
+        loadPokemonItens(offset, limit)
+    }
+})
